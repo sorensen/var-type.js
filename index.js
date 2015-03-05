722f2ef5a6c55acaec27e729cf604c5da49b1174
Beau Sorensen <mail@beausorensen.com>
2015-03-05 13:25:19 -0800

    Missing =

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var toString = Object.prototype.toString
 
 module.exports = function(x, test) {
-  var t toString.call(x).replace('[object ', '').replace(']', '')
+  var t = toString.call(x).replace('[object ', '').replace(']', '')
   if (test) return t === test
   return t
 }
