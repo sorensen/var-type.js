5873882880ae38ce1aca70b012a0cfa24a6bd277
Beau Sorensen <mail@beausorensen.com>
2015-03-16 14:03:26 -0700

    Use simpler replace method, slightly faster

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var toString = Object.prototype.toString
   , concat = Array.prototype.concat
 
 module.exports = function(x, tests) {
-  var t = toString.call(x).replace('[object ', '').replace(']', '')
+  var t = toString.call(x).replace(/\[object |\]/g, '')
     , args = slice.call(arguments, 1)
     , tests = concat.apply([], args)
 
